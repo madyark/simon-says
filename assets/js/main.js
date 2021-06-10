4adea005e7468b38d9be4272a0ff03ef1e1f8386
madyark <kozhakhmetovmadyar@gmail.com>
2021-06-10 18:49:36 +0000

    Add button memory features to the UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,14 @@ let resetStatus = false;
 // Selecting DOM attributes
 let displayButtons = document.getElementsByClassName("display-btn");
 let userButtons = document.getElementsByClassName("user-btn");
-console.log(userButtons);
 let gameInitializer = document.getElementById("init-game");
 let resetBtn = document.getElementById("reset-game");
 
-// Removing and add the disable property from user or display buttons (based on the arguements)
-let buttonDisableStatus = (buttonClass, status) => {
-    for (let i = 0; i < 9; i++) { // For each button selected with that specific class
+// Changing the disable property and background color from user buttons (based on the arguement)
+let userButtonStatus = (status) => {
+    for (let i = 0; i < userButtons.length; i++) {
         userButtons[i].disabled = status;
+        userButtons[i].classList = `user-btn user-btn-disabled-${status}`;
     }
 }
 
@@ -34,35 +34,57 @@ let removeHighlight = specificButton => {
     specificButton.innerHTML = `<button class="display-btn" disabled></button>`
 }
 
+// Add and remove the highlight on the specific button
+let displayChange = (buttonsArray, i) => {
+    if (resetStatus === true) { // Quick function exit if the reset button has been clicked
+        return;
+    }
+
+    userButtonStatus(true); // Disabling the user buttons before the highlight
+    highlightDisplay(buttonsArray[i]); // Add the button highlight
+
+    setTimeout(function() { 
+        removeHighlight(buttonsArray[i]); // Remove the button highlight
+    }, 400);
+
+    setTimeout(function() {
+        i++;
+
+        if (i === buttonsArray.length) {
+            userButtonStatus(false); // Enabling the user buttons after the removal of all the highlights
+            return; // Function escape
+        } else {
+            displayChange(buttonsArray, i); // Function recall for the rest of the buttonsArray
+        }
+    }, 600);
+}
+
 // Initializing the display of the buttons
 let initDisplay = () => {
     if (shownButtons.length >= 10 || resetStatus === true) {
-        buttonDisableStatus(userButtons, true); // Disabling the user buttons once the game is finished
-        gameInitializer.disabled = false; // Enabling the start-game button
+        userButtonStatus(true); // Disabling the user buttons once the game is finished
         return; 
     } 
 
     let ranNum = selectRandomNumber(1, 9);
 
     let chosenBtnDiv = document.getElementById(`d-btn-${ranNum}`);
+    shownButtons.push(chosenBtnDiv); // Adding the highlighted button to the shownButtons array
 
-    // Adding the highlighted button to the shownButtons array
-    shownButtons.push(chosenBtnDiv.id); 
+    
+    let iterator = 0;
+    displayChange(shownButtons, iterator); // Displaying the computer selected buttons from the array one by one, based on their index
+}
 
-    highlightDisplay(chosenBtnDiv);
-
-    setTimeout(function() { // Removes the button highlight and restarts the function
-        removeHighlight(chosenBtnDiv); 
-    }, 400);
-
-    setTimeout(function() {
-        initDisplay(); // Function recall
-    }, 800)
+for (let i = 0; i < userButtons.length; i++) { // Adding an event listener for each of the user buttons
+    userButtons[i].addEventListener("click", function() {
+        initDisplay();
+    });
 }
 
 gameInitializer.addEventListener("click", function() {
     gameInitializer.disabled = true; // Disabling the start-game button to prevent overclicking
-    buttonDisableStatus(userButtons, false); // Enabling the user buttons
+    resetBtn.disabled = false; // Enabling the reset button
     shownButtons = []; // Ensures that the array is clear
     initDisplay(); // Displays the buttons based on "Start game" selection
 });
@@ -72,8 +94,8 @@ resetBtn.addEventListener("click", function() {
     resetBtn.disabled = true;
 
     setTimeout(function() {
-        resetBtn.disabled = false;
         resetStatus = false; // Passing on the false value to the resetStatus again
+        gameInitializer.disabled = false;
     }, 500);
 });
 
