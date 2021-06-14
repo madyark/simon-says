6ed7b7757afc3e216cf025e97551679a9c95bacd
madyark <kozhakhmetovmadyar@gmail.com>
2021-06-14 14:09:49 +0000

    Add required import to the user-btn-highlight module

--- assets/js/modules/user-btn-highlight.js
+++ assets/js/modules/user-btn-highlight.js
@@ -1,3 +1,5 @@
+import {selectedButtons} from "./user-btn-select.js";
+
 // User selected button highlight 
 let userSelection = selectedButton => { // Adds and removes a highlight of the user selected button
     selectedButtons.push(selectedButton);
