2cd860034f30659cd422387ff0b35d811ebc2685
madyark <kozhakhmetovmadyar@gmail.com>
2021-06-14 14:04:45 +0000

    Add displayChange to the display-buttons module

--- assets/js/modules/display-buttons.js
+++ assets/js/modules/display-buttons.js
@@ -1,3 +1,7 @@
+import {resetStatus} from "./reset.js";
+import {userButtons} from "./user-btn-select.js";
+import userButtonStatus from "./user-btn-status.js";
+
 // Highlight display button
 let highlightDisplay = specificButton => {
     specificButton.innerHTML = `<button class="display-btn display-btn-highlight" disabled></button>`;
@@ -8,4 +12,32 @@ let removeHighlight = specificButton => {
     specificButton.innerHTML = `<button class="display-btn" disabled></button>`
 }
 
-export {highlightDisplay, removeHighlight};
+// Add and remove the highlight on the specific button
+let displayChange = (buttonsArray, i) => {
+    if (resetStatus === true) { // Quick function exit if the reset button has been clicked
+        return;
+    }
+
+    userButtonStatus(true, userButtons); // Disabling the user buttons before the highlight immediately
+    
+    setTimeout(function() {
+        highlightDisplay(buttonsArray[i]); // Add the button highlight
+    }, 150);
+
+    setTimeout(function() { 
+        removeHighlight(buttonsArray[i]); // Remove the button highlight
+    }, 400);
+
+    setTimeout(function() {
+        i++;
+
+        if (i === buttonsArray.length) {
+            userButtonStatus(false, userButtons); // Enabling the user buttons after the removal of all the highlights
+            return; // Function escape
+        } else {
+            displayChange(buttonsArray, i); // Function recall for the rest of the buttonsArray
+        }
+    }, 600);
+}
+
+export default displayChange;
