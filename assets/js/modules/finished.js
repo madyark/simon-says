1280396e67a0462ea5c1421589be4463a4d893bb
madyark <kozhakhmetovmadyar@gmail.com>
2021-06-14 13:09:06 +0000

    Create a new finished.js module

---
+++ assets/js/modules/finished.js
@@ -0,0 +1,14 @@
+import {removeHighlightDisplayCircle, removeHighlightUserCircles} from "./circles.js";
+import userButtonStatus from "./user-buttons-status.js";
+
+let gameInitializer = document.getElementById("init-game");
+let userButtons = document.getElementsByClassName("user-btn");
+
+let finished = () => {
+    removeHighlightDisplayCircle(); // Removing highlighted displayed circles
+    removeHighlightUserCircles(); // Removing highlighted user circles
+    userButtonStatus(true, userButtons); // Disabling the user buttons once the reset button has been clicked
+    gameInitializer.disabled = false;
+}
+
+export default finished;
