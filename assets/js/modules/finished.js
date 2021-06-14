fc83c62cb4d0713efab9c8d478b0c875a09c590b
madyark <kozhakhmetovmadyar@gmail.com>
2021-06-14 13:11:09 +0000

    Update finished module with extra comments

--- assets/js/modules/finished.js
+++ assets/js/modules/finished.js
@@ -8,7 +8,7 @@ let finished = () => {
     removeHighlightDisplayCircle(); // Removing highlighted displayed circles
     removeHighlightUserCircles(); // Removing highlighted user circles
     userButtonStatus(true, userButtons); // Disabling the user buttons once the reset button has been clicked
-    gameInitializer.disabled = false;
+    gameInitializer.disabled = false; // Enabling the start game button
 }
 
 export default finished;
