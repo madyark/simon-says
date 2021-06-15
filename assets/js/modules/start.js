2ec11a47e8a2b6fa7e6f5f09d8109e947ad46b88
madyark <kozhakhmetovmadyar@gmail.com>
2021-06-15 14:10:41 +0000

    Add startGame to the start module

--- assets/js/modules/start.js
+++ assets/js/modules/start.js
@@ -1,3 +1,16 @@
+import {resetBtn} from "./reset.js";
+import {clearShownButtons, initDisplay} from "./init-display.js";
+import {clearSelectedButtons} from "./user-btn-select.js";
+
 let gameInitializer = document.getElementById("init-game");
 
-export {gameInitializer};
+// Start game function
+let startGame = () => {
+    gameInitializer.disabled = true; // Disabling the start-game button to prevent overclicking
+    resetBtn.disabled = false; // Enabling the reset button
+    clearShownButtons(); // Clear the shown buttons array
+    clearSelectedButtons(); // Clear the selectedButtons array
+    initDisplay(); // Displays the buttons based on "Start game" selection
+}
+
+export {gameInitializer, startGame};
